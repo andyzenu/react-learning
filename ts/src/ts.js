d9e653fd19e92e64ec146a0b72e7f0e2dba9799f
Andy Gao <60496663+andyzenu@users.noreply.github.com>
2023-09-18 17:34:55 +1000

    ts

---
+++ ts/src/ts.js
@@ -0,0 +1,50 @@
+let c = false;
+c = true;
+function sumTotal(a, b) {
+    return a + b;
+}
+let gender;
+let types;
+let stringType;
+let anyType;
+let unknownType;
+unknownType = "this is a string";
+stringType = unknownType;
+console.log(sumTotal(1, 3));
+console.log(stringType);
+function fcd() {
+    console.log(1234);
+}
+function fcdnever() {
+    throw new Error("broken");
+}
+fcd();
+let obj;
+obj = { name: "monkey king", age: 12 };
+// add as many props as possible
+let newObj;
+newObj = { name: "monkey king", age: 100, gender: "male" };
+let newFc;
+newFc = (num1, num2) => num1 + num2;
+console.log(newFc(1, 2));
+// string array
+let stringArray; // let stringArray: Array<string>
+stringArray = ["monkey king", "monkey queen", "1"];
+// tuple
+let tupleNumType;
+tupleNumType = [1, 20];
+//enum type
+var Gender;
+(function (Gender) {
+    Gender[Gender["Male"] = 0] = "Male";
+    Gender[Gender["Female"] = 1] = "Female";
+    Gender[Gender["Other"] = 2] = "Other";
+})(Gender || (Gender = {}));
+let enumType;
+enumType = {
+    name: "Monkey King",
+    gender: Gender.Other
+};
+console.log(enumType.gender);
+let m;
+m = 4;
---
+++ ts/src/ts.ts
@@ -0,0 +1,85 @@
+let c: boolean = false
+c = true
+
+function sumTotal (a: number, b: number): number {
+  return a + b
+}
+
+let gender: "male" | "female"
+let types: string | number
+let stringType: string
+
+let anyType: any
+
+let unknownType: unknown
+
+unknownType = "this is a string"
+
+stringType = <string>unknownType
+
+console.log(sumTotal(1, 3))
+console.log(stringType)
+
+function fcd(): void {
+  console.log(1234)
+}
+
+function fcdnever(): never {
+  throw new Error("broken")
+}
+
+fcd()
+
+let obj: {
+  name: string,
+  age: number
+}
+
+obj = {name: "monkey king", age: 12}
+
+// add as many props as possible
+let newObj: {
+  name: string, 
+  [propName: string]: any
+}
+
+newObj = {name: "monkey king", age: 100, gender: "male"};
+
+let newFc: (a:number, b:number) => number
+
+newFc = (num1, num2) => num1 + num2
+
+console.log(newFc(1,2))
+
+// string array
+let stringArray: string[] // let stringArray: Array<string>
+stringArray = ["monkey king", "monkey queen", "1"]
+
+// tuple
+let tupleNumType: [number, number]
+tupleNumType = [1, 20]
+
+//enum type
+enum Gender {
+  Male,
+  Female,
+  Other
+} 
+
+let enumType: {
+  name: string,
+  gender: Gender
+}
+
+enumType = {
+  name: "Monkey King",
+  gender: Gender.Other
+}
+
+console.log(enumType.gender)
+
+// type aliases
+type myType = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+let m: myType
+
+m = 4
